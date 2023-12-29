5b5443201aebbb9423b83312b5709aaf00bec353
PainterHalver <hieptrang13042002@gmail.com>
2023-12-29 09:59:22 +0700

    style: add sidebar icons

--- web/components/SideBar.tsx
+++ web/components/SideBar.tsx
@@ -1,38 +1,61 @@
+import { getServerSession } from "next-auth";
 import Link from "next/link";
-import { HomeIcon } from "./icons/HomeIcon";
+import { AiOutlineFire } from "react-icons/ai";
+import { BsBookmark } from "react-icons/bs";
+import { GoHistory } from "react-icons/go";
+import { HiOutlineHome } from "react-icons/hi2";
+import { IoCheckboxOutline, IoSearchOutline } from "react-icons/io5";
+import { PiChatCircleDots } from "react-icons/pi";
 
-export default function SideBar() {
+export default async function SideBar() {
+  const session = await getServerSession();
+
   return (
     <aside className="border-r border-border fixed h-full overflow-scroll no-scrollbar bg-bg-primary lg:w-60 hidden lg:block lg:pt-4">
       <ul className="menu menu-md w-full p-0 [&_li>*]:rounded-none">
         <li className="menu-title">Discover</li>
         <li>
           <Link href={"/"}>
-            <HomeIcon /> Newly Updated
+            <HiOutlineHome className="text-lg" /> Newly Updated
           </Link>
         </li>
         <li>
-          <Link href={"/"}>Most loved</Link>
+          <Link href={"/"}>
+            <AiOutlineFire className="text-lg" /> Most loved
+          </Link>
         </li>
         <li>
-          <Link href={"/"}>Most discussed</Link>
+          <Link href={"/"}>
+            <PiChatCircleDots className="text-lg" /> Most discussed
+          </Link>
         </li>
         <li>
-          <Link href={"/"}>Search</Link>
+          <Link href={"/"}>
+            <IoSearchOutline className="text-lg" /> Search
+          </Link>
         </li>
       </ul>
-      <ul className="menu menu-md w-full p-0 [&_li>*]:rounded-none">
-        <li className="menu-title">Manage</li>
-        <li>
-          <Link href={"/"}>Bookmarks</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Bookmarks</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Follows</Link>
-        </li>
-      </ul>
+
+      {session && (
+        <ul className="menu menu-md w-full p-0 [&_li>*]:rounded-none">
+          <li className="menu-title">Manage</li>
+          <li>
+            <Link href={"/bookmarks"}>
+              <BsBookmark className="text-lg" /> Bookmarks
+            </Link>
+          </li>
+          <li>
+            <Link href={"/history"}>
+              <GoHistory className="text-lg" /> History
+            </Link>
+          </li>
+          <li>
+            <Link href={"/following"}>
+              <IoCheckboxOutline className="text-lg" /> Following
+            </Link>
+          </li>
+        </ul>
+      )}
     </aside>
   );
 }
