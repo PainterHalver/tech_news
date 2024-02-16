c7616933bd4840eaa5981ff3b46cea7057f5f20e
PainterHalver <hieptrang13042002@gmail.com>
2024-02-16 18:45:02 +0700

    feat(web): render AI generated content as markdown

--- web/app/posts/[post_id]/PostDescriptionAI.tsx
+++ web/app/posts/[post_id]/PostDescriptionAI.tsx
@@ -2,6 +2,7 @@
 
 import { Post } from "@/lib/types";
 import { HTMLAttributes } from "react";
+import Markdown from "react-markdown";
 
 type Props = {
   post: Post;
@@ -13,7 +14,9 @@ export default function PostDescriptionAI({ post, ...props }: Props) {
   return (
     <div className="border-l-[3px] border-primary p-2 gap-3 flex flex-col bg-bg-secondary mb-3" {...props}>
       <p className="text-text-primary">Tóm tắt AI (Gemini)</p>
-      <p className="text-text-primary font-[300] whitespace-pre-line text-[0.92rem]">{post.description_generated}</p>
+      <Markdown className="text-text-primary font-[300] whitespace-pre-line text-[0.92rem]">
+        {post.description_generated}
+      </Markdown>
     </div>
   );
 }
